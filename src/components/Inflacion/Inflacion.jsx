c035133c1c87171d39f09b22448716ac4ccf264f
Demian <aimedemian@gmail.com>
2024-12-18 12:07:46 -0300

    ajustados margenes en grafico linea

--- src/components/Inflacion/Inflacion.jsx
+++ src/components/Inflacion/Inflacion.jsx
@@ -17,7 +17,7 @@ export default function Inflacion({ data, months }) {
   // Crear chartData a partir de los datos proporcionados
   const chartData = months.map((month, index) => ({
     name: month,
-    value: Number(data[index]) || 0, // Convertir a número y manejar valores no numéricos
+    IPC: Number(data[index]) || 0, // Convertir a número y manejar valores no numéricos
   }));
 
   return (
@@ -35,7 +35,10 @@ export default function Inflacion({ data, months }) {
       <div className="w-full h-1/2 center-flex bg-gray-200 center-flex">
         <div className="wh90 rounded bg-gray-700 center-flex">
           <ResponsiveContainer width={"90%"} height={"90%"}>
-            <AreaChart width={500} height={300} data={chartData}>
+            <AreaChart
+              margin={{ top: 0, right: 0, left: -40, bottom: 0 }}
+              data={chartData}
+            >
               <XAxis
                 dataKey="name"
                 tick={{
@@ -50,7 +53,6 @@ export default function Inflacion({ data, months }) {
                   fontSize: "10px",
                   fontWeight: "600",
                 }}
-
               />
               <Tooltip
                 contentStyle={{
@@ -60,8 +62,13 @@ export default function Inflacion({ data, months }) {
                   color: "white",
                 }}
               />
-              <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
-              <Brush height={20} stroke="#555" />
+              <Area
+                type="monotone"
+                dataKey="IPC"
+                stroke="#8884d8"
+                fill="#8884d8"
+              />
+              <Brush height={10} stroke="#8884d8" />
             </AreaChart>
           </ResponsiveContainer>
         </div>
