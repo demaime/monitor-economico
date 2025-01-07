00bbd5ff6d2d68440c540b8ad503d99b634833b3
Demian <aimedemian@gmail.com>
2025-01-07 12:13:13 -0300

    nuevo test con global-css y modules separados

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,4 +1,4 @@
-import styles from "./style.module.css"; // Cambiar el nombre del archivo y la importación
+import styles from "./style.module.css";
 
 export default () => {
   return (
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import "@/styles/loader-globals.css"
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
