72c22bd24546c6023b61dd3c7ed4a0f0e64f24f7
Demian <aimedemian@gmail.com>
2025-01-07 14:37:10 -0300

    nuevo loader mas simple, sin problemas de clases

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,5 @@
 import styles from "./style.module.css";
 
 export default () => {
-  return (
-    <div className={styles.content}>
-      {[...Array(2)].map((_, i) => (
-        <div className={styles.bars} key={i}>
-          {[...Array(7)].map((_, b) => (
-            <div className={styles.bar} key={b}></div>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  return <div className={styles.loader}></div>;
 };
